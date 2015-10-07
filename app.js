1f043f93ca58e604bec848c7ce1e58109028acbf
pratiklodha95 <lodhapratik9@gmail.com>
2015-10-07 23:34:04 +0530

    index.html has the summary of the first task

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http=require("http");
 var express=require("express");
 var bodyParser=require("body-parser");
+var mysql=require('mysql');
 var app=express();
+
 
 app.set('port',process.env.PORT || 5000);
 
